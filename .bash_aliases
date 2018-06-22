61200ea11bc104df5db913f0c5d5e03aa092a953
zim <zim@desktop.desktop>
2018-06-22 01:11:37 -0400

    Updated config fir Debian 9.4

--- .bash_aliases
+++ .bash_aliases
@@ -1,10 +1,6 @@
 
 #export JAVA_HOME=/usr/bin/java
-export LD_LIBRARY_PATH=/usr/lib/jni
-export ACTIVATOR=/opt/activator
-export ANDROID=/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platforms-tools
-export DART=/usr/bin/dart
-export PUB=/usr/bin/pub
+#export PATH=$PATH:$JAVA
+
 export CMD=/home/zim/.bash
-export SCALA_HOME=/opt/scala/scala-2.11.7
-export PATH=$PATH:$ACTIVATOR:$ANDROID:$LD_LIBRARY_PATH:$DART:$PUB:$CMD:$SCALA_HOME/bin
+export PATH=$PATH:$CMD
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,9 +13,9 @@ main = xmonad $ defaultConfig {
   }
   `additionalKeys`
     [
-        ((mod4Mask, xK_p ), spawn "dmenu_run -fn 'Droid Sans Mono-9'")
-      , ((mod4Mask, xK_b ), spawn "feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/*")
-      , ((mod4Mask, xK_s ), spawn "scrot '%Y-%m-%d_%H.%m.%S_$wx$h.png' -e 'mv $f ~/Pictures/screenshot/'")
+        ((mod4Mask, xK_p ), spawn "dmenu_run -fn 'Monaco-11'")
+      , ((mod4Mask, xK_b ), spawn "feh --recursive --randomize --bg-fill /home/zim/Pictures/wallpapers/*")
+      , ((mod4Mask, xK_s ), spawn "scrot '%Y-%m-%d_%H.%m.%S_$wx$h.png' -e 'mv $f /home/zim/Pictures/screenshot/'")
       , ((mod4Mask, xK_F12),  spawn "/bin/systemctl poweroff -i")
-      , ((mod4Mask, xK_F11),  spawn "/bin/systemctl restart -i")
+      , ((mod4Mask, xK_F11),  spawn "/bin/systemctl reboot -i")
     ]
